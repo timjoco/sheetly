b4d0883f40e4e8251650d957cf0b5e50b55effbb
Tim <timjoco13@gmail.com>
2023-09-01 19:53:12 -0500

    cleaned up env config file

--- env-config.js
+++ env-config.js
@@ -1,11 +1,8 @@
 // env-config.js
 import { config } from 'dotenv';
 
-// Load environment variables from .env file
 config();
-
 // Use this to log env variables
 // console.log('Environment variables:', process.env);
 
-// Export the loaded environment variables for use in your application
 export default process.env;
